da344f80f10752fa3733f6610a113613fb11486f
山鸡 <1570354680@qq.com>
2019-12-06 22:51:56 +0800

    视图暂存

--- project/info/modules/Student/views.py
+++ project/info/modules/Student/views.py
@@ -1,12 +1,8 @@
 from . import index_blu
 from flask import request
 
-@index_blu.route('/index')
-def index():
-    return 'index'
 
-
-@index_blu.route('/teacher/ownInfo/<int:tid>')
+@index_blu.route('/teacher/MyTeacher/<int:tid>')
 def index1(tid):
     print(tid)
     return 'index'
